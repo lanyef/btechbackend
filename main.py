c8b2fdab260641a23cfb9146b8bc232ad186ba6e
lanye <lumanye02@gmail.com>
2024-06-02 13:57:00 +0100

    first commit for project

---
+++ main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, File, UploadFile
+import tensorflow as tf
+import numpy as np
+import uvicorn
+from io import BytesIO
+from PIL import Image
+
+app = FastAPI()
+
+
+loaded_model = tf.keras.models.load_model("../models/3")
+
+loaded_model.summary()
+
+# def read_file_as_image(data) -> np.ndarray:
+#     image = np.array(Image.open(BytesIO(data)))
+#     return image
+
+CLASS_NAMES = ['Tomato___Bacterial_spot',
+ 'Tomato___Early_blight',
+ 'Tomato___Late_blight',
+ 'Tomato___Leaf_Mold',
+ 'Tomato___Septoria_leaf_spot',
+ 'Tomato___Spider_mites Two-spotted_spider_mite',
+ 'Tomato___Target_Spot',
+ 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+ 'Tomato___Tomato_mosaic_virus',
+ 'Tomato___healthy']
+
+def read_file_as_image(bytes):
+    image = Image.open(BytesIO(bytes))
+    image = image.resize((256, 256))
+    image = image.convert('RGB')
+    image = np.array(image)
+    image = np.expand_dims(image, axis=0)
+    return image
+
+@app.post('/predict')
+async def prediction(
+        file: UploadFile = File(...)
+):
+    bytes = await file.read()
+
+    image = read_file_as_image(bytes)
+    
+    predictions = loaded_model.predict(image)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host='localhost', port=8000)
